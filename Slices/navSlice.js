476f879495d7d159fb47cdebdb6e285873319eba
bluVshal <blu.vishal@gmail.com>
2024-06-17 20:07:17 +0400

    Added slice and store for redux implementation

---
+++ Slices/navSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    origin: null, //where are you right now?
+    destination: null, //where do you want to go?
+    travelTimeInformation: null // how much time it will take to reach destination
+}
+
+export const navSlice = createSlice({
+    name: 'nav',
+    initialState,
+    reducer:{
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.origin = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.orgin = action.payload;
+        }
+    },
+});
+
+
+export const{ setOrigin, setDestination, setTravelTimeInformation} = navSlice.actions;
+
+//grab data from Data Layer
+// Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.setTravelTimeInformation;
+
+export default navSlice.reducer;
